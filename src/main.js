c8feae7b065bcd61a18b6709d5dc659bd5848116
Tejas <tbhavsar121@gmail.com>
2022-09-06 23:50:02 +0530

    refactor camera and plane

--- src/main.js
+++ src/main.js
@@ -9,87 +9,61 @@ import { TextureLoader } from "three";
 const width = window.innerWidth;
 const height = window.innerHeight;
 var scene = new THREE.Scene();
+var clock = new THREE.Clock();
+scene.background = new THREE.Color(0xe0e0e0);
+scene.fog = new THREE.Fog(0xe0e0e0, 20, 100);
 scene.background = new THREE.Color("#808080");
-var camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-var cameraTarget = { x: 0, y: 0, z: 0 };
-camera.position.y = 70;
-camera.position.z = 500;
-camera.rotation.x = (-15 * Math.PI) / 180;
-var renderer = new THREE.WebGLRenderer();
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(width, height);
-document.body.appendChild(renderer.domElement);
+var camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.25,
+  100
+);
+camera.position.set(-5, 3, 10);
+camera.lookAt(new THREE.Vector3(0, 2, 0));
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 
-var light = new THREE.AmbientLight(0xffffff, 1.3);
-light.position
-  .set(camera.position.x, camera.position.y + 500, camera.position.z + 500)
-  .normalize();
-scene.add(light);
-const controls = new OrbitControls(camera, renderer.domElement);
-var geometry = new THREE.PlaneBufferGeometry(100, 100, 256, 256);
-var material = new THREE.MeshLambertMaterial({
-  color: 0xffffff,
-  map: new THREE.TextureLoader().load("../textures/sand.jpg"),
-});
-var plain = new THREE.Mesh(geometry, material);
-plain.rotation.x = -Math.PI / 2;
-scene.add(plain);
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+hemiLight.position.set(0, 20, 0);
+scene.add(hemiLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff);
+dirLight.position.set(0, 20, 10);
+scene.add(dirLight);
+
+const mesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(2000, 2000),
+  new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
+);
+mesh.rotation.x = -Math.PI / 2;
+scene.add(mesh);
+
 const loader = new GLTFLoader();
 loader.load(
   "../model/RobotExpressive.glb",
   function (gltf) {
     var model = gltf.scene;
     scene.add(model);
+
+    createGUI(model, gltf.animations);
   },
   undefined,
   function (e) {
     console.error(e);
   }
 );
-var perlin = new Perlin();
-plain.peak = 60;
-plain.smooth = 500;
-console.log(plain);
-function refreshVertices() {
-  var vertices = plain.geometry.attributes.position.array;
-  for (var i = 0; i <= vertices.length; i += 3) {
-    vertices[i + 2] =
-      plain.peak *
-      perlin.noise(
-        (plain.position.x + vertices[i]) / plain.smooth,
-        (plain.position.z + vertices[i + 1]) / plain.smooth
-      );
-  }
-  plain.geometry.attributes.position.needsUpdate = true;
-  plain.geometry.computeVertexNormals();
-}
-
-const gui = new GUI();
-const plainFolder = gui.addFolder("plain");
-
-plainFolder.add(plain.scale, "x", 0, 10).name("width");
-plainFolder.add(plain.scale, "y", 0, 10).name("height");
-plainFolder.add(plain, "smooth", 10, 600);
-plainFolder.add(plain, "peak", 10, 600);
-plainFolder.open();
-const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(controls, "reset").name("Reset Camera");
-cameraFolder.add(camera.position, "z", 0, 100);
-cameraFolder.open();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+const controls = new OrbitControls(camera, renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
-  refreshVertices();
+
   render();
 }
-var clock = new THREE.Clock();
-var movementSpeed = 0.1;
-// function update() {
-//   var delta = clock.getDelta();
-//   plain.rotation.z += movementSpeed * delta;
-//   refreshVertices();
-// }
 
 function render() {
   renderer.render(scene, camera);
