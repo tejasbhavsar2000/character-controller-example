561bb4bc3883c750fbaf122535a4b2a4a830618e
Tejas <tbhavsar121@gmail.com>
2022-09-17 19:27:10 +0530

    added dat.gui

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import * as CANNON from "cannon-es";
 import * as OIMO from "oimo";
 import { threeToCannon, ShapeType } from "three-to-cannon";
-import CannonDebugger from "cannon-es-debugger";
+
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 console.log(OIMO.BODY_GHOST);
@@ -15,6 +15,7 @@ const height = window.innerHeight;
 var scene = new THREE.Scene();
 var clock = new THREE.Clock();
 var mixer;
+
 scene.background = new THREE.Color(0xe0e0e0);
 scene.fog = new THREE.Fog(0xe0e0e0, 20, 100);
 scene.background = new THREE.Color("#808080");
@@ -38,7 +39,6 @@ dirLight.position.set(0, 20, 10);
 scene.add(dirLight);
 
 const groundPhysMat = new CANNON.Material();
-
 const groundBody = new CANNON.Body({
   //shape: new CANNON.Plane(),
   //mass: 10
@@ -55,6 +55,19 @@ const groundMat = new THREE.MeshBasicMaterial({
   map: new THREE.TextureLoader().load("../textures/sand.jpg"),
   side: THREE.DoubleSide,
 });
+
+const box = new THREE.BoxGeometry(5, 5, 5);
+const boxMat = new THREE.LineBasicMaterial({ color: 0xffffff });
+const boxMesh = new THREE.Mesh(box, boxMat);
+
+const boxBody = new CANNON.Body({
+  //shape: new CANNON.Plane(),
+  mass: 1,
+  shape: new CANNON.Box(new CANNON.Vec3(2.5, 2.5, 2.5)),
+});
+boxBody.position.set(10, 5, 10);
+world.addBody(boxBody);
+scene.add(boxMesh);
 const groundMesh = new THREE.Mesh(groundGeo, groundMat);
 scene.add(groundMesh);
 
@@ -70,13 +83,29 @@ controls.enablePan = false;
 controls.maxPolarAngle = Math.PI / 2 - 0.05;
 controls.update();
 document.body.appendChild(renderer.domElement);
-const cannonDebugger = new CannonDebugger(scene, world);
+
 const loader = new GLTFLoader();
 const axisHelper = new THREE.AxesHelper(9);
 scene.add(axisHelper);
+const gui = new GUI();
+
+const physics = {
+  togglePhysics: false,
+};
+
+const PhysicsFolder = gui.addFolder("Physics");
+PhysicsFolder.add(physics, "togglePhysics")
+  .listen()
+  .onChange((value) => {
+    value ? world.addBody(hitBody) : world.removeBody(hitBody);
+  });
+
+PhysicsFolder.open();
+
 var characterController;
 let hitBody;
 let model;
+const hitboxPhyMat = new CANNON.Material();
 loader.load(
   "../model/Robot.glb",
   function (gltf) {
@@ -92,9 +121,10 @@ loader.load(
       mass: 1,
       shape: new CANNON.Box(new CANNON.Vec3(1.5, 2.2, 1)),
       position: new CANNON.Vec3(0, 3, 0),
-      //material: hitboxPhyMat,
+      material: hitboxPhyMat,
     });
-    world.addBody(hitBody);
+    hitBody.linearDamping = 0.01;
+
     console.log(model);
     scene.add(model);
 
@@ -144,18 +174,82 @@ document.addEventListener(
   false
 );
 function reset() {
+  // Position
+  boxBody.position.setZero();
+  boxBody.previousPosition.setZero();
+  boxBody.interpolatedPosition.setZero();
+  boxBody.initPosition.setZero();
+
+  // orientation
+  boxBody.quaternion.set(0, 0, 0, 1);
+  boxBody.initQuaternion.set(0, 0, 0, 1);
+  boxBody.previousQuaternion.set(0, 0, 0, 1);
+  boxBody.interpolatedQuaternion.set(0, 0, 0, 1);
+
+  // Velocity
+  boxBody.velocity.setZero();
+  boxBody.initVelocity.setZero();
+  boxBody.angularVelocity.setZero();
+  boxBody.initAngularVelocity.setZero();
+
+  // Force
+  boxBody.force.setZero();
+  boxBody.torque.setZero();
+
+  // Sleep state reset
+  boxBody.sleepState = 0;
+  boxBody.timeLastSleepy = 0;
+  boxBody._wakeUpAfterNarrowphase = false;
+  boxBody.position.set(10, 5, 10);
+  // Position
+  hitBody.position.setZero();
+  hitBody.previousPosition.setZero();
+  hitBody.interpolatedPosition.setZero();
+  hitBody.initPosition.setZero();
+
+  // orientation
+  hitBody.quaternion.set(0, 0, 0, 1);
+  hitBody.initQuaternion.set(0, 0, 0, 1);
+  hitBody.previousQuaternion.set(0, 0, 0, 1);
+  hitBody.interpolatedQuaternion.set(0, 0, 0, 1);
+
+  // Velocity
+  hitBody.velocity.setZero();
+  hitBody.initVelocity.setZero();
+  hitBody.angularVelocity.setZero();
+  hitBody.initAngularVelocity.setZero();
+
+  // Force
+  hitBody.force.setZero();
+  hitBody.torque.setZero();
+
+  // Sleep state reset
+  hitBody.sleepState = 0;
+  hitBody.timeLastSleepy = 0;
+  hitBody._wakeUpAfterNarrowphase = false;
   hitBody.position.set(1, 20, 1);
+  characterController.updateCameraTarget(
+    hitBody.position.x,
+    hitBody.position.y
+  );
 }
+
+var mat1_ground = new CANNON.ContactMaterial(groundPhysMat, hitboxPhyMat, {
+  friction: 10,
+  restitution: 0,
+});
+world.addContactMaterial(mat1_ground);
 function animate() {
   let delta = clock.getDelta();
   if (characterController) {
     characterController.update(delta, keysPressed);
   }
-
+  boxMesh.position.copy(boxBody.position);
+  boxMesh.quaternion.copy(boxBody.quaternion);
   groundMesh.position.copy(groundBody.position);
   groundMesh.quaternion.copy(groundBody.quaternion);
   if (model) {
-    if (hitBody.position.y < -20) {
+    if (hitBody.position.y < -20 || hitBody.position.y > 20) {
       console.log(hitBody.position.y);
       reset();
     } else {
@@ -174,7 +268,7 @@ function animate() {
   }
   if (delta > 0) world.step(delta);
   controls.update();
-  cannonDebugger.update();
+
   render();
   requestAnimationFrame(animate);
 }
